67b1b91341a0754c8dede88613fe57222119393a
Akmal Djumakhodjaev <akmal@binary.com>
2022-04-14 17:47:23 +0800

    Add basic platformer with assets

---
+++ src/phaser/basic-platform.js
@@ -0,0 +1,116 @@
+import Phaser from "phaser";
+
+let player;
+let stars;
+let platforms;
+let cursors;
+let movingPlatform;
+
+class playPlatformerGame extends Phaser.Scene {
+  constructor() {
+    super("PlayPlatformerGame");
+  }
+  preload() {
+    this.load.image("sky", "src/assets/sky.png");
+    this.load.image("ground", "src/assets/platform.png");
+    this.load.image("star", "src/assets/star.png");
+    this.load.spritesheet("dude", "./src/assets/dude.png", {
+      frameWidth: 32,
+      frameHeight: 48,
+    });
+  }
+
+  collectStar(player, star) {
+    star.disableBody(true, true);
+  }
+
+  create() {
+    this.add.image(400, 300, "sky");
+
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, "ground").setScale(2).refreshBody();
+
+    // platforms.create(600, 400, 'ground');
+    // platforms.create(50, 250, 'ground');
+    // platforms.create(750, 220, 'ground');
+
+    movingPlatform = this.physics.add.image(400, 400, "ground");
+
+    movingPlatform.setImmovable(true);
+    movingPlatform.body.allowGravity = false;
+    movingPlatform.setVelocityX(50);
+
+    player = this.physics.add.sprite(100, 450, "dude");
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+      key: "left",
+      frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "turn",
+      frames: [{ key: "dude", frame: 4 }],
+      frameRate: 20,
+    });
+
+    this.anims.create({
+      key: "right",
+      frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    stars = this.physics.add.group({
+      key: "star",
+      repeat: 11,
+      setXY: { x: 12, y: 0, stepX: 70 },
+    });
+
+    stars.children.iterate(function (child) {
+      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    });
+
+    this.physics.add.collider(player, platforms);
+    this.physics.add.collider(player, movingPlatform);
+    this.physics.add.collider(stars, platforms);
+    this.physics.add.collider(stars, movingPlatform);
+
+    this.physics.add.overlap(player, stars, this.collectStar, null, this);
+  }
+  
+  update() {
+    if (cursors.left.isDown) {
+      player.setVelocityX(-160);
+
+      player.anims.play("left", true);
+    } else if (cursors.right.isDown) {
+      player.setVelocityX(160);
+
+      player.anims.play("right", true);
+    } else {
+      player.setVelocityX(0);
+
+      player.anims.play("turn");
+    }
+
+    if (cursors.up.isDown && player.body.touching.down) {
+      player.setVelocityY(-330);
+    }
+
+    if (movingPlatform.x >= 500) {
+      movingPlatform.setVelocityX(-50);
+    } else if (movingPlatform.x <= 300) {
+      movingPlatform.setVelocityX(50);
+    }
+  }
+}
+
+export default playPlatformerGame;
