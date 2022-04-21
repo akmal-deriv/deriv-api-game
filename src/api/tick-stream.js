44baad6f4552cbf9ed4eb08d0ba47e5186bbae2b
mahdiyeh-fs <mahdiyeh@firstsource.tech>
2022-04-21 10:33:04 +0430

    remove hardcoded token

--- src/api/tick-stream.js
+++ src/api/tick-stream.js
@@ -1,7 +1,7 @@
 const DerivAPIBasic = require('@deriv/deriv-api/dist/DerivAPIBasic');
 
 const api = new DerivAPIBasic({ endpoint: 'frontend.binaryws.com', lang: 'EN', app_id: 31635 });
-const token = '3DwG8iZet26fHaV';
+const token = process.env.REACT_APP_DERIV_API_KEY;
 
 export default async function setEnemyLevelSession() {
     try {
