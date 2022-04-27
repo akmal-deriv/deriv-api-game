60472775e7249cef293132052c645981d82bab1c
mahdiyeh-fs <mahdiyeh@firstsource.tech>
2022-04-28 02:48:35 +0430

    add login with token page

--- src/api/tick-stream.js
+++ src/api/tick-stream.js
@@ -1,7 +1,7 @@
 const DerivAPIBasic = require('@deriv/deriv-api/dist/DerivAPIBasic');
 
 const api = new DerivAPIBasic({ endpoint: 'frontend.binaryws.com', lang: 'EN', app_id: 31635 });
-const token = process.env.REACT_APP_DERIV_API_KEY;
+const token = sessionStorage.getItem('user_token');
 
 export default async function setEnemyLevelSession() {
     try {
